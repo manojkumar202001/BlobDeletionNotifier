15afcb8d520bca9ee0287c7ba834f4cc21d7d22a
manojkumar202001 <128701573+manojkumar202001@users.noreply.github.com>
2025-08-30 17:57:50 +0530

    Update __init__.py

--- BlobDeleteEvent/__init__.py
+++ BlobDeleteEvent/__init__.py
@@ -1,23 +1,22 @@
-import os
 import logging
 import azure.functions as func
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
+import os
 
 def main(event: func.EventGridEvent):
     logging.info(f"Event received: {event.get_json()}")
 
     event_data = event.get_json()
-    blob_url = event_data.get("data", {}).get("url", "Unknown URL")
-    logging.info(f"Blob URL: {blob_url}")
+    blob_url = event_data.get("url", "Unknown URL")
+    blob_name = blob_url.split("/")[-1]
 
-    # Send email via SendGrid
     sendgrid_api_key = os.environ.get("SENDGRID_API_KEY")
     message = Mail(
         from_email="manojselva592285@outlook.com",
         to_emails="manojselva7094@gmail.com",
         subject="Azure Blob Deleted Notification",
-        html_content=f"<p>Blob Deleted: {blob_url}</p>"
+        html_content=f"<h3>Blob Deleted Alert</h3><p><b>Blob Name:</b> {blob_name}</p><p><b>Blob URL:</b> {blob_url}</p>"
     )
 
     try:
